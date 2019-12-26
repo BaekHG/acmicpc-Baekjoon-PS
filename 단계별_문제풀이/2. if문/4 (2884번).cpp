77fea296da5e3369fb3f852bff54d03d717252d5
brobaik <hhyungi94@gmail.com>
2019-12-26 18:33:36 +0900

    step_2

--- 단계별_문제풀이/2. if문/4 (2884번).cpp
+++ 단계별_문제풀이/2. if문/4 (2884번).cpp
@@ -3,23 +3,44 @@
 using namespace std;
 
 int main(){
-	int h;
-	int m;
-		
+	int h,m;
+
 	cin >> h >> m;
-	
-	
-	
-	if( (m%60) -45 >=0){
-		cout << (h%24) <<" "<< (m%60)-45;
-	}else{
-		if( (h%24 - 1) < 0 ){
-			h = 24 + (h%24 -1);
-		}else{
-			h = h%24 -1;
-		}
-		cout << h <<" " << 15+ (m%60);
+
+	m = m - 45;
+
+	if(m<0){
+
+		h = h - 1;
+		if(h<0) h = h + 24;
+		m = m + 60;
 	}
-	
+
+	cout << h << ' ' << m;
+
 	return 0;
-} 
+}
+
+//다른 풀이
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int h,m;
+
+	cin >> h >> m;
+
+	m = m - 45;
+
+	if(m<0){
+
+		h = h - 1;
+		if(h<0) h = h + 24;
+		m = m + 60;
+	}
+
+	cout << h << ' ' << m;
+
+	return 0;
+}
