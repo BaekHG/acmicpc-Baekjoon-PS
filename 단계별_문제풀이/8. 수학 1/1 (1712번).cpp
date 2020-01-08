dae2f54820a21dedcc270942680ec8584433d0f5
brobaik <hhyungi94@gmail.com>
2020-01-08 16:04:38 +0900

    step8

--- 단계별_문제풀이/8. 수학 1/1 (1712번).cpp
+++ 단계별_문제풀이/8. 수학 1/1 (1712번).cpp
@@ -4,26 +4,15 @@ int main(){
 	
 	//a: 고정비용 , b: 가변 비용, c: 가격 
 	int a,b,c;
-	int cnt=1;
 	cin >> a >> b >> c;
-	
-	
-//	while(a+(b*cnt) >= c*cnt){
-	
 	if(c<=b) {
 		cout << -1;
 		return 0;
 	}
 	else{
-		while(a >= (c-b)*cnt){
-			++cnt;
-			if((c-b)*cnt >2100000000){
-				cout <<-1;
-				return 0;
-			}
-		} 
+		cout << a/ (c-b) + 1;
+		return 0;
 	}	
-	cout << cnt;
    	return 0;
 }
-//2100000000
+
